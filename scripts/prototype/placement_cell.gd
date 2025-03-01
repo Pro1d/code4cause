f407301a83d0eeeb07c3b1ce9aacf8876454688c
Pro1d <you@example.com>
2025-03-01 17:05:07 +0100

    tile delete anim

--- scripts/prototype/placement_cell.gd
+++ scripts/prototype/placement_cell.gd
@@ -37,12 +37,25 @@ func place() -> void:
 	
 func delete() -> void:
 	is_set = false
-	var tween := create_tween()
-	tween.tween_interval(0.1 - global_position.z/3)
-	tween.tween_property(self, "global_position:y", -30.0, 1.0) \
-		.set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_QUAD)
-	tween.tween_callback(queue_free)
 	reset()
+	
+	var shake_magnitude := 0.07 # m
+	var shake_duration := 0.1 # s
+	var gpos := global_position
+	var tween := create_tween()
+	tween.tween_interval(randf() * 0.4)
+	var shake_offset := Vector3(1, 0, 0).rotated(Vector3.UP, randf() * TAU)
+	var shake_count := 8
+	for i in range(shake_count):
+		var shake_factor := pow(float(i + 1) / shake_count, 2.0)
+		var shake := shake_offset * shake_magnitude * shake_factor
+		var t := tween.parallel() if i > 0 else tween
+		t.tween_property(self, "global_position", gpos + shake, shake_duration) \
+			.set_delay(i * shake_duration).set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_SINE)
+		shake_offset = shake_offset.rotated(Vector3.UP, deg_to_rad(180.0 + randf_range(-20.0, 20.0)))
+	tween.parallel().tween_property(self, "global_position:y", -5.0, 1.0) \
+		.set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_QUAD).set_delay(shake_count * shake_duration * .85)
+	tween.tween_callback(queue_free)
 	
 func redraw_child(new_is_set:bool, scene: PackedScene) -> void:
 	is_set = new_is_set
