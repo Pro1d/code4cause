5c2eeef9338524599a5e163178f3503d1cfccc85
DoubleSept <contact@doublesept.fr>
2025-03-04 21:38:31 +0100

    new row generator

--- scripts/prototype/row_generator.gd
+++ scripts/prototype/row_generator.gd
@@ -4,6 +4,7 @@ extends Node
 var initial_cells_length := 2
 
 var total_rows := 0
+var consecutive_empty_lines := 0
 var tile_straight := preload("res://resources/placeholder/tile_straight.tscn")
 var tile_turn := preload("res://resources/placeholder/tile_turn.tscn")
 var tile_block := preload("res://resources/placeholder/tile_block.tscn")
@@ -24,7 +25,12 @@ func get_row(height: int) -> Array[PrefabTile]:
 		var tmp : Array = buffer.pop_front()
 		for row in len(tmp):
 			blocks[row] = tmp[row]
-	elif total_rows > 3 and total_rows % 4 == 1:
+		consecutive_empty_lines = 0
+	elif total_rows > 3 and(
+		(consecutive_empty_lines >= 3)
+		or (total_rows > 15 and consecutive_empty_lines >= 2)
+		or (total_rows > 30 and consecutive_empty_lines >= 1)
+		):
 		var prefab := PrefabTile.random_prefab(total_rows)
 
 		# compute a delta so we can place the prefab somewhere within the height's range
@@ -41,17 +47,9 @@ func get_row(height: int) -> Array[PrefabTile]:
 			for row in len(col):
 				tmp[row + delta] = col[row]
 			buffer.push_back(tmp)
-
-	#if total_rows > 3 and total_rows % 3 == 1:
-		#if randi()%3!=0:
-			#var indexes: Array = [1,2] if randi()%2==0 else [0,3]
-			#for i in indexes as Array[int]:
-				#blocks[i] = PrefabTile.new(tile_block)
-			#
-	#if total_rows % 9 == 8:
-		#var indexes: Array = [1,2,3] if randi()%2==0 else [0,1,2]
-		#for i in indexes as Array[int]:
-			#blocks[i] = PrefabTile.new(tile_block)
+		consecutive_empty_lines = 0
+	else:
+		consecutive_empty_lines += 1
 
 	total_rows += 1
 	return blocks
